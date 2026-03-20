27366f1b9e4c8a0c1185d0e2e39bd4e5a14fdbad
Jessiebrw1223 <blackfranco39@gmail.com>
2026-03-20 16:15:46 -0500

    Update FeaturedProducts.tsx

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -20,7 +20,7 @@ const item = {
   },
 };
 
-const API_BASE_URL = "http://127.0.0.1:10000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const FeaturedProducts = () => {
   const [products, setProducts] = useState<Product[]>([]);
